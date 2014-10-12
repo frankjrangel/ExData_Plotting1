0f181b7f826f621d3d29e531c942e2e3efa58ab9
Francisco Rangel <frankjrangel@gmail.com>
2014-10-12 18:05:27 -0430

    Second Plot

---
+++ plot2.R
@@ -0,0 +1,18 @@
+#Read the dataframe
+data <- read.table("household_power_consumption.txt", sep=";", header=TRUE)
+
+#Subset desired dates
+data <-data[which(data[,1]=="2/2/2007" | data[,1]=="1/2/2007"),]
+
+#Set time column
+data$mtime <- strptime(paste(data[,1], data[,2], sep=" "), format="%d/%m/%Y %H:%M:%S")
+
+#Set numeric values to work with plot()
+data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
+
+#Open device and plot data
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+
+plot(data$mtime, data$Global_active_power, type ="l", ylab ="Global Active Power (kilowatts)", xlab ="", cex.axis =.68, cex.lab = .75)
+
+dev.off()
